65e43224d4519b6a74f74b7d0a4c2a0082b923bf
Ruff50 <cbolac@gmail.com>
2022-09-06 20:51:26 +0400

    page d'accueil de l'application

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
-use App\Entity\SolBaseStock;
-use App\Entity\Inventaire;
-use App\Entity\InventaireSolBaseStock;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Doctrine\ORM\Mapping\OrderBy;
 
@@ -19,8 +17,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage()
+    public function homepage(ProductRepository $productRepository): Response
     {
-        return $this->render('home.html.twig');
+        $products = $productRepository->findBy([], [], 3);
+        return $this->render('home.html.twig', [
+            'products' => $products,
+        ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,7 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Product;
-
+use Bluemmb\Faker\PicsumPhotosProvider;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -13,15 +13,17 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
+        $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
         for ($i = 0; $i < 50; $i++) {
             $product = new Product();
             $product->setNom($faker->sentence(3));
             $product->setDesignation($faker->sentence());
             $product->setPrix(mt_rand(100, 1000));
-            $product->setAvis($faker->sentence());
+            $product->setAvis($faker->paragraph());
             $product->setCodeart($faker->sentence(1));
             $product->setQte(mt_rand(1, 100));
             $product->setActif(mt_rand(0, 1));
+            $product->setImage($faker->imageUrl(200, 200, true));
 
 
             $manager->persist($product);
